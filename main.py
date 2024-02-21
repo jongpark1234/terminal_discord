9cdf22717fc14a02429e650df87a57e7c57673ff
JongPark <jongpark1234@naver.com>
2024-02-21 18:04:23 +0900

    v 1.1
    + Add emoji search command
    + Add silent mode command
    - Remove selecting by guild/channel id
    - Remove showing by guild/channel id
    + object type that saving on client int (id) -> discord object

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 import sys
 import os
+from typing import Union
 from discord.ext import commands
 from dpyConsole import Console
+
+from datetime import timedelta
 
 from config import APPID, TOKEN
 
@@ -18,23 +21,22 @@ class DavidChoi(commands.Bot):
             application_id=APPID
         )
 
-        self.curGuild = None
-        self.curChannel = None
+        self.curGuild: discord.guild.Guild = None
+        self.curChannel: discord.channel.TextChannel = None
+        self.isSilent: bool = False
 
     async def on_ready(self):
-        activity = discord.Game('개뻘짓')
+        activity = discord.Game('')
         await self.change_presence(status=discord.Status.online, activity=activity)
+        os.system('cls')
         print('Bot is ready.')
     
     async def on_message(self, message: discord.Message) -> None:
-        if message.author.bot:
-           return
-        if message.channel.id == self.curChannel:
-            printChannelHistory(formatMessage(message))
+        if message.channel.id == self.curChannel.id:
+            await printChannelHistory(message.channel)
 
 client = DavidChoi()
 console = Console(client)
-historyList = []
 
 @console.command()
 async def help():
@@ -42,65 +44,53 @@ async def help():
             help - print this message.
                         
             show guilds - Show list that all guilds.            
-            show channels - Shows all the channels for a selected guild. {'' if client.curGuild else '( Now Unavailable )'}
+            show channels - Shows all the channels for a selected guild.
             
             select guild [guild id] - select the guild to send message.         
             select channel [channel id] - select the channel to send message.
             
-            send [message] - send message to selected channel. {'' if client.curChannel else '( Now Unavailable )'}
+            send [message] - send message to selected channel.
 
             status - print status of now selected guild and channel.
             cls - clear the command screen.
 
             chat - gets the last 50 messages for current channel.
+
+            silent - if it is on, clear the terminal immediately when sending a chat.
+
+            search [name] - Locate the emoji of the searched name.
     ''')
-    historyList.append(0)
 
 @console.command()
-async def show(target):
-    if 'guild' in target or target in 'guild':
+async def show(target_name):
+    if 'guild' in target_name or target_name in 'guild':
         for idx, guild in enumerate(client.guilds):
             print(f'{idx + 1}. {guild.name} ( {guild.id} )')
-        historyList.append(1)
 
-    elif 'channel' in target or target in 'channel':
-        for idx, channel in enumerate(client.get_guild(client.curGuild).text_channels):
+    elif 'channel' in target_name or target_name in 'channel':
+        for idx, channel in enumerate(client.curGuild.text_channels):
             print(f'{idx + 1}. {channel.name} ( {channel.id} )')
-        historyList.append(2)
 
     else:
         print('Invalid Command.')
 
 @console.command()
-async def select(target, target_id):
+async def select(target_name, target_id):
     target_id = int(target_id)
-    if 'guild' in target or target in 'guild':
+    if 'guild' in target_name or target_name in 'guild':
         guilds = client.guilds
-
-        if target_id in map(lambda x: x.id, guilds): # exact id
-            client.curGuild = target_id
+        if target_id < len(guilds): # index expression
+            client.curGuild = guilds[max(0, target_id - 1)]
             client.curChannel = None
-            historyList.append(3)
-            print(f'Guild Selected.')
-        elif target_id < len(guilds): # index expression
-            client.curGuild = guilds[max(0, target_id - 1)].id
-            client.curChannel = None
-            historyList.append(3)
-            print(f'Guild Selected.')
+            print(f'Guild Selected. {client.curGuild.name}')
         else:
             print('Guild Not Found.')
 
-    elif 'channel' in target or target in 'channel':
-        channels = client.get_guild(client.curGuild).text_channels
-
-        if target_id in channels:
-            client.curChannel = target_id # exact id
-            historyList.append(4)
-            print('Channel Selected.')
-        elif target_id < len(channels): # index expression
-            client.curChannel = channels[max(0, target_id - 1)].id
-            historyList.append(4)
-            print('Channel Selected.')
+    elif 'channel' in target_name or target_name in 'channel':
+        channels = client.curGuild.text_channels
+        if target_id < len(channels): # index expression
+            client.curChannel = channels[max(0, target_id - 1)]
+            print(f'Channel Selected. {client.curChannel.name}')
         else:
             print('Channel Not Found.')
 
@@ -110,47 +100,58 @@ async def select(target, target_id):
 @console.command()
 async def send(*message): 
     if client.curChannel:
-        message = ' '.join(message)
-        channel = client.get_channel(client.curChannel)
-
-        await channel.send(message)
-        await printChannelHistory(formatMessage(channel))
-        historyList.append(5)
-
+        await client.curChannel.send(' '.join(message))
     else:
         print('Channel Do Not Selected.')
 
 @console.command()
 async def status():
     print(f'''Status ----
-          Selected Guild : {client.get_guild(client.curGuild).name if client.curGuild else '---'} ( {client.curGuild} )
-          Selected Channel : {client.get_channel(client.curChannel).name if client.curChannel else '---'} ( {client.curChannel} )
+          Selected Guild : {client.curGuild.name if client.curGuild else '---'}
+          Selected Channel : {client.curChannel.name if client.curChannel else '---'}
     ''')
-    historyList.append(6)
 
 @console.command()
 async def cls():
     os.system('cls')
-    historyList.append(7)
 
 @console.command()
 async def chat():
     if client.curChannel:
-        await printChannelHistory(client.get_channel(client.curChannel))
+        await printChannelHistory(client.curChannel)
     else:
         print('Select the channel.')
 
+@console.command()
+async def silent():
+    client.isSilent = not client.isSilent
+    print(f'Silent Mode = {client.isSilent}')
 
+@console.command()
+async def search(name):
+    try:
+        if len(name) < 2:
+            print('Please enter at least two letters.')
+            return
+        
+        emoji_dict = { x.name.lower(): f'<:{x.name}:{x.id}>' for x in client.emojis }
+        filtered_emoji_list = list(filter(lambda x: name in x, emoji_dict.keys()))
 
+        if len(filtered_emoji_list) == 0:
+            print('No search results found.')
+            return
 
-
+        for emoji in filtered_emoji_list:
+            print(emoji_dict[emoji])
+    except Exception as e:
+        print(e)
 
 async def printChannelHistory(channel):
     for message in [message async for message in channel.history(limit=50)][::-1]:
         print(formatMessage(message))
 
 def formatMessage(message: discord.Message) -> str:
-    return f'ㆍ{message.guild.name} #{message.channel.name} {message.author.name}: {message.content} ( {message.created_at.strftime("%Y/%m/%d %H:%M:%S")} )'
+    return f'ㆍ{message.guild.name} #{message.channel.name} {message.author.name}: {message.content} ( {(message.created_at + timedelta(hours=9)).strftime("%Y/%m/%d %H:%M:%S")} ) {list(map(lambda x: x.url, message.attachments)) if message.attachments else ""} {message.stickers}'
     
 
 if __name__ == '__main__':
